c24b9fccccfed7604d8ffae5a8df94b8bb7664e3
ALLUK <lam83@bk.ru>
2021-09-25 19:50:11 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool accessAllowed;
+
+            string storePassword = "qwerty";
+            string enteredPassword = Console.ReadLine();
+
+            if (storePassword==enteredPassword)
+            {
+                accessAllowed = true;
+            }
+            else
+            {
+                accessAllowed = false;
+            }
+            Console.WriteLine(accessAllowed);
+            Console.ReadLine();
+        }
+    }
+}
